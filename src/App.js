c2af464ea40d887a214d33f02c54978408742ddc
braj <brajksingh8582@gmail.com>
2023-12-06 22:10:21 +0530

    Added Routes

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
+import About from './components/About';
+import Home from './components/Home';
 
 function App() {
   return (
     <>
-    <Navbar title="3"/>
-    <div className="container my-3" >
-
-    <TextForm heading = "This is Form Heading"/>
-
+    <Navbar title="This Is Title"/>
+    <Routes>
+      <Route path="/" element={ <TextForm></TextForm>}/>
+      <Route path="/about" element={ <About></About>}/>
+      <Route path="/home" element={ <Home></Home>}/>
+    </Routes>
     
-    </div>
     </>
   );
 }
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,9 @@
 import React from 'react'
 
-export default function About() {
+export default function Home() {
   return (
     <div>
-      This is ABOUT
+      THIS IS HOME
     </div>
   )
 }
